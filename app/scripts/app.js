5412f80c9c87aa0c4a882a7b30eca26d30a4fc71
Karthik Rao <karthikrao@Karthiks-Air.Home>
2017-09-04 20:29:33 +0530

    -Implement getting thumbnail by chaining

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,20 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function(response){
+      addSearchHeader(response.query);
+      return getJSON(response.results[0]);
+    })
+    .catch(function(){
+      throw Error('Search Request Error');
+    })
+    .then(function(planetData){
+      createPlanetThumb(planetData);
+    })
+    .catch(function(error){
+      console.log(error);
+      addSearchHeader('unknown');
+    })
   });
 })(document);
